8a58396a1595402962b188e43af94a7bc6cf4a22
soph1428 <sophiagroth16@gmail.com>
2024-09-02 23:46:17 -0500

    update

--- findtheinvisiblecheese.js
+++ findtheinvisiblecheese.js
@@ -4,37 +4,39 @@ cheesesfound = document.getElementById("cheesesfound"),
 fastesttime = document.getElementById("fastesttime"),
 stopwatch = document.getElementById("stopwatch"),
 cNote = document.createElement("audio"), stopwatchInterval = "",
-yum = document.createElement("audio"),
-xDifference = "", yDifference = "", distance = "", detectMousePos = false
+yum = document.createElement("audio"), distance = ""
 cNote.volume = 0, yum.src = `yum.mp3`
 if (localStorage.getItem("Cheeses Found")) {
     cheesesfound.textContent = `Cheeses Found: ${localStorage.getItem("Cheeses Found")}`
 } if (localStorage.getItem("Fastest Time")) {
     fastesttime.textContent = `Fastest Time: ${localStorage.getItem("Fastest Time")}`
 } function changeDistances(x, y) {
-    xDifference = x, yDifference = y
+    var xDifference = x, yDifference = y
     distance = Math.sqrt(xDifference * xDifference + yDifference * yDifference)
-} start.onclick = function() {
+} start.onclick = function(e) {
     start.style.display = "none"
+    document.getElementById(`directions`).style.display = "none"
     cNote.src = "cNote.mp3", cNote.loop = true, cNote.play()
-    startStopwatch(), changeDistances("", "")
-    cheese.style.left = `${Math.floor(Math.random() * (((innerWidth - 100) - 100 + 1) + 100))}px`
-    cheese.style.top = `${Math.floor(Math.random() * (((innerHeight - 100) - 100 + 1) + 100))}px`
-    detectMousePos = true
+    startStopwatch(), cheese.style.left = `${Math.floor(Math.random() * ((innerWidth - cheese.width) + 1))}px`
+    cheese.style.top = `${Math.floor(Math.random() * ((innerHeight - cheese.height) + 1))}px`
+    move(e)
 }; document.addEventListener("mousemove", move)
+document.querySelectorAll(`*`).forEach(elem => elem.addEventListener(`mousemove`, function() {
+    if (cNote.volume != 1) elem.style.cursor = `unset`
+}))
 document.addEventListener("touchmove", (e) => {move(e.targetTouches[0])})
 document.addEventListener("touchend", () => {cNote.volume = 0})
 function move(e) {
-    document.documentElement.style.cursor = "unset"
-    if (detectMousePos) {
+    e.target.style.cursor = "unset"
+    if (cNote.loop) {
         changeDistances(Number(cheese.style.left.slice(0, cheese.style.left.length - 2)) + cheese.width / 2 - e.clientX,
         Number(cheese.style.top.slice(0, cheese.style.top.length - 2)) + cheese.height / 2 - e.clientY)
         if (distance > 500) cNote.volume = 0.1
         else if (distance < 400 && distance > 300) cNote.volume = 0.3
         else if (distance < 200 && distance > 100) cNote.volume = 0.6
-        else if (distance < cheese.width / 4) {
+        else if (distance < cheese.width / 3) {
             cNote.volume = 1
-            document.documentElement.style.cursor = "pointer"
+            e.target.style.cursor = "pointer"
         }
     }
 } function startStopwatch() {
@@ -53,11 +55,11 @@ function move(e) {
         }
     }, 1000)
 } document.addEventListener("click", () => {
-    if (document.documentElement.style.cursor == "pointer") {
-        cheese.style.display = "unset"
+    if (cNote.volume == 1 && start.style.display == `none`) {
+        cheese.style.display = "unset", cNote.volume = 0
         cheese.style.animation = "cheeseAnimation 0.7s ease-out forwards"
         clearInterval(stopwatchInterval)
-        detectMousePos = false, cNote.loop = false
+        false, cNote.loop = false
         if (fastesttime.textContent == "Fastest Time: 0:00") {
             localStorage.setItem("Fastest Time", stopwatch.textContent.slice(11, stopwatch.textContent.length))
             fastesttime.textContent = `Fastest Time: ${localStorage.getItem("Fastest Time")}`
@@ -70,7 +72,7 @@ function move(e) {
         cheesesfound.textContent = `Cheeses Found: ${Number(cheesesfound.textContent.slice(cheesesfound.textContent.indexOf(":") + 2, cheesesfound.textContent.length)) + 1}`
         localStorage.setItem("Cheeses Found", Number(cheesesfound.textContent.slice(cheesesfound.textContent.indexOf(":") + 2, cheesesfound.textContent.length)))
         setTimeout(() => {yum.play()}, 500)
-        setTimeout(() => {start.style.display = "unset"
+        setTimeout(() => {start.style.display = "block"
             cheese.style.display = ""
             cheese.style.animation = ""
         }, 1500)
